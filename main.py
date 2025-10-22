93d8a104258a2a4136bd94a971db8eabb570fedf
Lucas Lopes <66391853+lucaslopes-ti@users.noreply.github.com>
2025-10-22 13:46:37 +0000

    codigos de exemplo em sala

---
+++ main.py
@@ -0,0 +1,50 @@
+# print("hello world")
+
+# printa na tela o seu nome
+"""
+nome = "SENAI"
+idade = 83
+print(nome, idade)
+"""
+# imprima o seu nome e idade
+"""
+a = 10
+b = 5
+print("Soma:", a + b)
+
+name = input("Digite o nome: ")
+print("Olá", name)
+
+idade = int(input("Digite a sua idade: "))
+if idade > 18:
+    print("Voce é maior idade")
+else:
+    print("Voce é menor")
+
+
+for i in range(5):
+    print("Contador: ", i)
+
+EXERCICIO: IMPRIMA TODOS NUMEROS PARES DE 0 A 10
+
+
+def saudacao(nome):
+    print("Bem-vindo", nome)
+
+saudacao("Lucas")
+
+EXERCICIO: CRIE UMA FUNCAO QUE RECEBE DOIS NUMEROS E RETORNA A SOMA DELES
+
+
+import random
+
+cores = ["azul", "verde", "vermelho", "rosa"]
+cor_sorteada = random.choice(cores)
+print("A cor escolhida: ", cor_sorteada)
+
+"""
+
+
+
+
+
